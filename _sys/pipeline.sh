5c3e9b0378d12141b4b736734275d1c9fd7b0e66
Cole Mlostek <147189238+cmlostek@users.noreply.github.com>
2025-03-12 17:54:07 -0400

    chore: update to pipleine.sh to upgrade pip before installing dependencies.

--- _sys/pipeline.sh
+++ _sys/pipeline.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Run this script to install all the dependencies for the project on your local machine
 function pipeline {
+      pip install --upgrade pip # Upgrade pip before installing dependencies
+
       pip install fastapi
       pip install "uvicorn[standard]"
       pip install sqlalchemy
