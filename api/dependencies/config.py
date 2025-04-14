94a9a7cbdd1e3c904508511bcf0a6a49c402ca0d
Cole Mlostek <147189238+cmlostek@users.noreply.github.com>
2025-04-14 11:15:39 -0400

    chore: made the dbname and password DEFAULT so that no information of other accounts were leaked.

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -1,8 +1,8 @@
 class conf:
     db_host = "localhost"
-    db_name = "sandwich_maker_api"
+    db_name = "my_dbname_here"
     db_port = 3306
     db_user = "root"
-    db_password = "12229291"
+    db_password = "my_password_here"
     app_host = "localhost"
     app_port = 8000
