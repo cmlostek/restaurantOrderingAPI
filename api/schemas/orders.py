c5407eb866305cbcb0675eeaf544b38ffc69ac79
Cole Mlostek <147189238+cmlostek@users.noreply.github.com>
2025-04-14 11:13:30 -0400

    chore: update to the schemas to match our defined table names feat: added and implemented necessary schemas for the new tables and the values necessary.

--- api/schemas/orders.py
+++ api/schemas/orders.py
@@ -1,28 +1,16 @@
-from datetime import datetime
-from typing import Optional
 from pydantic import BaseModel
-from .order_details import OrderDetail
+from typing import Optional
+from datetime import datetime
 
+class OrderSchema(BaseModel):
+    order_id: int
+    user_id: int
+    dish_id: int
+    user_name: str
+    order_date: datetime
+    order_status: str
+    total_price: float
+    order_details: Optional[str]
 
-
-class OrderBase(BaseModel):
-    customer_name: str
-    description: Optional[str] = None
-
-
-class OrderCreate(OrderBase):
-    pass
-
-
-class OrderUpdate(BaseModel):
-    customer_name: Optional[str] = None
-    description: Optional[str] = None
-
-
-class Order(OrderBase):
-    id: int
-    order_date: Optional[datetime] = None
-    order_details: list[OrderDetail] = None
-
-    class ConfigDict:
-        from_attributes = True
+    class Config:
+        orm_mode = True
