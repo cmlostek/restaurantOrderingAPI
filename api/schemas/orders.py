58329fc53388271a55da948eeb3c1dacd26a9d74
ToyaOkey <cooltoya12@gmail.com>
2025-04-22 12:02:57 -0400

    orders schemas edited

--- api/schemas/orders.py
+++ api/schemas/orders.py
@@ -6,21 +6,11 @@ class OrderSchema(BaseModel):
     order_id: int
     user_id: int
     dish_id: int
-    user_name: str
     order_date: datetime
     order_status: str
     total_price: float
     order_details: Optional[str]
+    is_guest: bool = False
 
     class Config:
         orm_mode = True
-
-class GuestOrderItem(BaseModel):
-    product_id: int
-    quantity: int
-
-class GuestOrderCreate(BaseModel):
-    items: List[GuestOrderItem]
-    dish_id: int
-    total_price: float
-    guest_email: str
