d9d14b5054361be4667f0c7f10b974aab624338c
ToyaOkey <cooltoya12@gmail.com>
2025-04-24 21:57:55 -0400

    fix: removed quantity from orders

--- api/models/users.py
+++ api/models/users.py
@@ -13,7 +13,7 @@ user_review = Table(
 class User(Base):
     __tablename__ = 'users'
     user_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    payment_id = Column(Integer, ForeignKey("payment.payment_id"))
+    payment_id = Column(Integer, ForeignKey("payments .payment_id"))
     user_name = Column(String(100), nullable=False)
     email = Column(String(100), unique = True, nullable=False)
     phone_number = Column(Integer, unique = True, nullable=False)
