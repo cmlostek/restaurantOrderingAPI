cc20920763db98a23da3d2d59532d1c31d20c898
ToyaOkey <cooltoya12@gmail.com>
2025-05-02 11:51:28 -0400

    fix(promotions): fixed test cases for promotions

--- api/tests/test_promotions.py
+++ api/tests/test_promotions.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi.testclient import TestClient
 from ..controllers import orders as controller
 from ..main import app
@@ -14,62 +16,91 @@ def seeded_client():
     return client
 
 
-
-def test_read_all_promotions(db_session, mocker):
-    mock_get_all_promotions = mocker.patch.object(controller, 'read_all_promotions', return_value=[Promotion(id=1, name="Test Promotion")])
-
-    response = client.get("/promotions/")
-
+def test_read_all_promotions(seeded_client):
+    response = seeded_client.get("/promotions/")
     assert response.status_code == 200
-    assert response.json() == [{"id": 1, "name": "Test Promotion"}]
+    assert isinstance(response.json(), list)
+    assert len(response.json()) > 0
+    assert all("promotion_id" in promo and "code" in promo for promo in response.json())
 
-def test_read_promotion_by_id(db_session, mocker):
-    mock_get_promotion_by_id = mocker.patch.object(controller, 'read_promotion', return_value=Promotion(id=1, name="Test Promotion"))
 
-    response = client.get("/promotions/1")
 
+def test_read_promotion_by_id(seeded_client):
+    response = seeded_client.get("/promotions/1")
     assert response.status_code == 200
-    assert response.json() == {"id": 1, "name": "Test Promotion"}
+    assert response.json() ==  {
+        "code": "SPRING10",
+        "description": "10% off all dishes during the Spring season",
+        "discount_percentage": 10,
+        "valid_until": "2025-06-01T23:59:59",
+        "promotion_id": 1
+    }
 
-def test_create_new_promotion(db_session, mocker):
-    mock_create_promotion = mocker.patch.object(controller, 'create_new_promotion', return_value=Promotion(id=1, name="Test Promotion"))
 
+
+def test_create_new_promotion(seeded_client):
     promotion_request = {
-        "name": "Test Promotion",
+        "code": "TEST",
         "description": "This is a test promotion",
-        "discount_percentage": 10
+        "discount_percentage": 20,
+        "valid_until": "2025-05-02T15:41:01.115Z"
     }
 
-    response = client.post("/promotions/", json=promotion_request)
+    response = seeded_client.post("/promotions/", json=promotion_request)
     assert response.status_code == 200
-    assert response.json() == {"id": 1, "name": "Test Promotion"}
+    data = response.json()
+    assert data["code"] == "TEST"
+    assert data["description"] == "This is a test promotion"
+    assert data["discount_percentage"] == 20
+    assert "promotion_id" in data
+    assert "valid_until" in data
 
-def test_update_existing_promotion(db_session, mocker):
-    mock_update_promotion = mocker.patch.object(controller, 'update_promotion', return_value=Promotion(id=1, name="Updated Promotion"))
+
+
+
+def test_update_existing_promotion(seeded_client):
+    promotion_update = {
+        "code": "SPRING20",
+        "description": "20% off all dishes during the Spring season",
+        "discount_percentage": 20,
+        "valid_until": "2025-06-01T23:59:59"
+    }
+
+    response = seeded_client.put("/promotions/1", json=promotion_update)
+    assert response.status_code == 200
+    data = response.json()
+    assert data["code"] == "SPRING20"
+    assert data["description"] == "20% off all dishes during the Spring season"
+    assert data["discount_percentage"] == 20
+    assert data["promotion_id"] == 1
+    assert data["valid_until"] == "2025-06-01T23:59:59"
+
+
+def test_delete_promotion(seeded_client):
 
     promotion_request = {
-        "promotion_id": 1,
-        "name": "Updated Test Promotion",
-        "description": "This is an updated test promotion",
-        "discount_percentage": 15
+        "code": "TEST_DELETE",
+        "description": "This is a test promotion to delete",
+        "discount_percentage": 20,
+        "valid_until": "2025-05-02T15:41:01.115Z"
     }
 
-    response = client.put("/promotions/1", json=promotion_request)
+    response = seeded_client.post("/promotions/", json=promotion_request)
     assert response.status_code == 200
-    assert response.json() == {"id": 1, "name": "Updated Promotion"}
+    promotion_id = response.json()["promotion_id"]
+    assert promotion_id is not None
 
-def test_delete_promotion(db_session, mocker):
-    mock_delete_promotion = mocker.patch.object(controller, 'delete_promotion', return_value=True)
 
-    response = client.delete("/promotions/1")
+    response = seeded_client.delete("/promotions/5")
     assert response.status_code == 200
-    assert response.json() == {"detail": "Promotion deleted successfully"}
+    data = response.json()
 
-def test_read_promotion_by_id_not_found(db_session, mocker):
-    mock_get_promotion_by_id = mocker.patch.object(controller, 'read_promotion', return_value=None)
+    assert data["code"] == "TEST_DELETE"
+    assert data["description"] == "This is a test promotion to delete"
+    assert data["discount_percentage"] == 20
+    assert data["promotion_id"] == 5
+    assert "valid_until" in data
 
-    response = client.get("/promotions/999")
 
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Promotion not found"}
+
 
