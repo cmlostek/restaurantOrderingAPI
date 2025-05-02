da813ebe40d17e30f522df27b35f72f77e0a8809
ToyaOkey <cooltoya12@gmail.com>
2025-05-02 12:53:53 -0400

    chore(tests): configured os path for pytest to find project route

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,3 +1,10 @@
+import sys, os
+root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+if root not in sys.path:
+    sys.path.insert(0, root)
+
+
+
 import pytest
 from fastapi.testclient import TestClient
 
